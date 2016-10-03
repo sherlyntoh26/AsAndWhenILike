f0de2fc1b2a62c96535ec3c67fe76eb465302a44
sherlyntoh26 <sherlyn_sardiine@hotmail.com>
2016-10-04 03:39:45 +0800

    testing for newOrderTransaction. SUCCESSFUL ~!

--- project/src/project/Connection.java
+++ project/src/project/Connection.java
@@ -6,9 +6,15 @@ public class Connection {
 	private Cluster cluster;
 	private Session session;
 	
+	public Connection(){
+		cluster = Cluster.builder().addContactPoints("127.0.0.1").build();
+		session = cluster.connect();
+	}
+	
 	public void connect(String node, String keyspaceName){
 		cluster = Cluster.builder().addContactPoints(node).build();
 		session = cluster.connect(keyspaceName);
+		//session = cluster.connect();
 	}
 	
 	public Session getSession(){
